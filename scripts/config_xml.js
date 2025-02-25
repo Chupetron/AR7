26ae35545dae6de4b6093ab4695d74f79c40b56f
Chupetron <23chupete@gmail.com>
2025-02-25 20:20:47 -0300

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,54 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2024-12-18 09:16:20 p. m." dc:source="Camtasia,24.1.2,esn" dc:title="ute4fun480" tscDM:firstFrame="ute4fun480_First_Frame.png" tscDM:project="ute4fun">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="328533"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="480" stDim:w="854"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="es-ES">Spanish</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="15600" xmpDM:duration="29200" tscDM:boundingPoly="0,152;407,152;407,327;0,327;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="207300" xmpDM:duration="27600" tscDM:boundingPoly="700,322;839,322;839,400;700,400;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="234933"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="129400" xmpDM:duration="15470" tscDM:boundingPoly="26,386;225,386;225,463;26,463;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="149100" xmpDM:duration="15400" tscDM:boundingPoly="673,389;854,389;854,465;673,465;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="5" xmpDM:startTime="169030" xmpDM:duration="15670" tscDM:boundingPoly="30,383;229,383;229,460;30,460;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="6" xmpDM:startTime="207300" xmpDM:duration="27600" tscDM:boundingPoly="547,384;686,384;686,463;547,463;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="281067"/></rdf:li><rdf:li><rdf:Description xmpDM:label="7" xmpDM:startTime="243800" xmpDM:duration="36370" tscDM:boundingPoly="31,380;230,380;230,456;31,456;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="206300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="8" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="335,332;633,332;633,360;335,360;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="104067"/></rdf:li><rdf:li><rdf:Description xmpDM:label="9" xmpDM:startTime="84100" xmpDM:duration="18430" tscDM:boundingPoly="25,386;224,386;224,463;25,463;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="10" xmpDM:startTime="108300" xmpDM:duration="16370" tscDM:boundingPoly="27,385;226,385;226,461;27,461;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="11" xmpDM:startTime="187470" xmpDM:duration="18330" tscDM:boundingPoly="27,383;226,383;226,460;27,460;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="45300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="12" xmpDM:startTime="285730" xmpDM:duration="41800" tscDM:boundingPoly="31,380;230,380;230,456;31,456;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="206300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="13" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="443,225;583,225;583,261;443,261;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="75967"/></rdf:li><rdf:li><rdf:Description xmpDM:label="14" xmpDM:startTime="16200" xmpDM:duration="28870" tscDM:boundingPoly="517,170;785,170;785,284;517,284;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="206300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="15" xmpDM:startTime="209130" xmpDM:duration="25770" tscDM:boundingPoly="33,19;232,19;232,96;33,96;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="14533"/></rdf:li><rdf:li><rdf:Description xmpDM:label="16" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="262,161;633,161;633,193;262,193;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="125167"/></rdf:li><rdf:li><rdf:Description xmpDM:label="17" xmpDM:startTime="15070" xmpDM:duration="29730" tscDM:boundingPoly="476,239;810,239;810,442;476,442;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="206300"/></rdf:li><rdf:li><rdf:Description xmpDM:label="18" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="228,387;362,387;362,453;228,453;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="145367"/></rdf:li><rdf:li><rdf:Description xmpDM:label="19" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="0,321;259,321;259,345;0,345;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="165267"/></rdf:li><rdf:li><rdf:Description xmpDM:label="20" xmpDM:startTime="46730" xmpDM:duration="28740" tscDM:boundingPoly="9,106;332,106;332,136;9,136;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="185433"/></rdf:li><rdf:li><rdf:Description xmpDM:label="21" xmpDM:startTime="49700" xmpDM:duration="25770" tscDM:boundingPoly="673,389;854,389;854,465;673,465;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="14533"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introducción" xmpDM:startTime="0" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="INICIO" xmpDM:startTime="14533" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="UTE" xmpDM:startTime="45300" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="MOTORUTE" xmpDM:startTime="75967" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="3" tscDM:imageoffset="0" tscDM:imagerect="225, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="COMBUSTIBLEUTE" xmpDM:startTime="104067" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="4" tscDM:imageoffset="0" tscDM:imagerect="300, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="PLATAFORMAUTE" xmpDM:startTime="125167" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="5" tscDM:imageoffset="0" tscDM:imagerect="375, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="RUEDASUTE" xmpDM:startTime="145367" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="6" tscDM:imageoffset="0" tscDM:imagerect="450, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="TRANSMISIONUTE" xmpDM:startTime="165267" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="7" tscDM:imageoffset="0" tscDM:imagerect="525, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="AEROUTE" xmpDM:startTime="185433" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="8" tscDM:imageoffset="0" tscDM:imagerect="600, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="MAPA" xmpDM:startTime="206300" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="9" tscDM:imageoffset="0" tscDM:imagerect="675, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="DAYTONA" xmpDM:startTime="234933" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="10" tscDM:imageoffset="0" tscDM:imagerect="750, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="SUNSET" xmpDM:startTime="281067" tscDM:image="ute4fun480_Thumbnails.png" tscDM:imageindex="11" tscDM:imageoffset="0" tscDM:imagerect="825, 0, 75, 42"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="es-ES"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="ute4fun480.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="ute4fun480_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="ute4fun480_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
